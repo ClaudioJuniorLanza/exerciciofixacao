cfe3e0865f24f58677d314f55ae5aff362bd500a
Claudio Lanca <clanca@uolinc.com>
2022-02-12 12:29:28 -0300

    Cria opção para filtros por campos

--- src/main/java/br/com/letscode/cadastrorebeldes/enums/OpcaoFiltro.java
+++ src/main/java/br/com/letscode/cadastrorebeldes/enums/OpcaoFiltro.java
@@ -1,4 +1,19 @@
 package br.com.letscode.cadastrorebeldes.enums;
 
 public enum OpcaoFiltro {
+
+    NOME("nome"),
+    IDADE("idade"),
+    RACA("raca");
+
+    private String descricao;
+
+    OpcaoFiltro(String descricao){
+        this.descricao = descricao;
+    }
+
+    public String getDescricao(){
+        return descricao;
+    }
+
 }
