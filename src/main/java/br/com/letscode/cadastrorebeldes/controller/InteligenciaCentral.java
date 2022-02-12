114a83c5e9351306e3b6f60fde43b4d614a8317d
Claudio Lanca <clanca@uolinc.com>
2022-02-12 12:28:11 -0300

    Atualiza forma de escrita de arquivo e impressão de dados em tela

--- src/main/java/br/com/letscode/cadastrorebeldes/controller/InteligenciaCentral.java
+++ src/main/java/br/com/letscode/cadastrorebeldes/controller/InteligenciaCentral.java
@@ -1,16 +1,20 @@
-package br.com.letscode.cadastrorebeldes;
+package br.com.letscode.cadastrorebeldes.controller;
 
+import br.com.letscode.cadastrorebeldes.domain.Rebelde;
+import br.com.letscode.cadastrorebeldes.enums.TipoRebelde;
 import lombok.Cleanup;
 
-import java.io.FileNotFoundException;
-import java.io.PrintWriter;
-import java.io.UnsupportedEncodingException;
+import java.io.*;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Random;
+import java.util.Scanner;
 
 public class InteligenciaCentral {
 
-    boolean verificaAceitacaoIC(String racaRebelde){
+    public boolean verificaAceitacaoIC(String racaRebelde){
         Random r = new Random();
         TipoRebelde[] values = TipoRebelde.values();
         boolean existeRebelde = Arrays.asList(values).stream().
@@ -19,19 +23,39 @@ public class InteligenciaCentral {
             System.out.println("Tipo de rebelde não existente. Por favor, tente novamente conforme opções apresentadas.");
             return false;
         }
-
         String racaAceitavel = Arrays.asList(values).get(r.nextInt(values.length)).getDescricao();
         System.out.println(racaAceitavel);
         return racaAceitavel.equals(racaRebelde);
     }
 
-    public void imprimirRebeldesAdmitidos(RebeldesController cadastroRebelde) throws FileNotFoundException, UnsupportedEncodingException {
-        @Cleanup PrintWriter writer = new PrintWriter("lista-admitidos.txt", "UTF-8");
-        writer.println("LISTA DOS REBELDES ADMITIDOS");
-        writer.println("Nome: " + cadastroRebelde.getNome() + " - "
-                     + "Idade: " + cadastroRebelde.getIdade() + " - "
-                     + "Raça: " + cadastroRebelde.getTipoRebelde().getDescricao());
-        writer.println("--------------------------------------------------------------------");
-        System.out.println("Rebelde adicionado a lista com sucesso!");
+    public void imprimirRebeldesAdmitidos(List<Rebelde> rebeldesList){
+        rebeldesList.stream().forEach(rebelde -> System.out.println(rebelde.toString()));
+        escreverDadosArquivo(rebeldesList);
+
+    }
+
+    public void escreverDadosArquivo(List<Rebelde> rebeldeList) {
+
+        File file = new File("lista-admitidos.txt");
+        if (file.exists()){
+            try {
+                @Cleanup FileWriter fileWriter = new FileWriter(file.getAbsoluteFile(), true);
+                @Cleanup PrintWriter printWriter = new PrintWriter(fileWriter);
+                rebeldeList.stream().
+                        forEach(rebelde -> printWriter.println(rebelde.toString()));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }else{
+            try {
+                @Cleanup PrintWriter writer = new PrintWriter("lista-admitidos.txt", "UTF-8");
+                rebeldeList.stream().
+                        forEach(rebelde -> writer.println(rebelde.toString()));
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
